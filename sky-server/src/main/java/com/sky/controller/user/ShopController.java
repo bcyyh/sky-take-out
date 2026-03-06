a978b4fff52d4d87e6aabe257245bcca340bb6b7
bcyyh <18559838792@163.com>
2026-03-06 18:42:17 +0800

    店铺营业状态设置功能开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,30 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+//返回的所有东西都要直接转成 JSON 给前端
+@RequestMapping("/user/shop")
+
+@Slf4j
+public class ShopController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    public static final String KEY = "SHOP_STATUS";
+
+    /**
+     * 获取营业状态
+     * @return
+     */
+    @GetMapping("/status")
+
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取营业状态:{}", status == 1? "营业中" : "打烊中");
+        return Result.success(status);
+    }
+}
