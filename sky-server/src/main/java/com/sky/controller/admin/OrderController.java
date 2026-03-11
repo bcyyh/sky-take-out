23dfbb5f5915940fab9780d297866256c0ae2770
bcyyh <18559838792@163.com>
2026-03-11 15:56:54 +0800

    day09订单详情代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -6,10 +6,12 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,17 @@ public class OrderController {
         OrderStatisticsVO orderStatisticsVO = orderService.statistics();
         //返回的为VO 即data数据
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 订单详情
+     */
+    @GetMapping("/details/{id}")
+    @ApiOperation("订单详情")
+    public Result<Object> details(@PathVariable("id") Long id){
+        log.info("订单详情，订单id为：{}", id);
+        //Data中有数据 一般返回VO数据
+        OrderVO orderVO = orderService.details(id);
+        return Result.success(orderVO);
     }
 }
