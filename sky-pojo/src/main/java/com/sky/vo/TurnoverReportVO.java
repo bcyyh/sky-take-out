1e8a4d4db11c2e7d212727faaf1cb0bb593cd8b6
bcyyh <18559838792@163.com>
2026-03-13 10:51:13 +0800

    day11营业额统计功能代码

--- sky-pojo/src/main/java/com/sky/vo/TurnoverReportVO.java
+++ sky-pojo/src/main/java/com/sky/vo/TurnoverReportVO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 
@@ -14,6 +15,7 @@ import java.io.Serializable;
 public class TurnoverReportVO implements Serializable {
 
     //日期，以逗号分隔，例如：2022-10-01,2022-10-02,2022-10-03
+
     private String dateList;
 
     //营业额，以逗号分隔，例如：406.0,1520.0,75.0
--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -68,4 +69,12 @@ public interface OrderMapper {
     //花括号中为传入的参数
     @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+
+    /**
+     * 根据动态条件统计营业额数据
+     * @param map
+     * @return
+     */
+    Double sumBYMap(Map map);
 }
